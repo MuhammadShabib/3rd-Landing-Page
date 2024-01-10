7147b85ae3d46582561900506121083c39b104bf
Shabib <mohammadshabib@gmail.com>
2024-01-10 21:22:06 +0430

    adding counter part

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Dark Mode
 
 document.addEventListener('alpine:init', () => {
-        Alpine.data('darkMode', () => ({
+    Alpine.data('darkMode', () => ({
             darkMode: true,
             init() {
                 if (localStorage.theme === 'dark') {
@@ -19,9 +19,34 @@ document.addEventListener('alpine:init', () => {
                 })
             }
 
+        })),
+        Alpine.data('countDown', () => ({
+            days: '00',
+            hours: '00',
+            minutes: '00',
+            seconds: '00',
+            init() {
+                let endDate = new Date(2024, 1, 11).getTime();
+                setInterval(() => {
+                    let newDate = new Date().getTime();
+                    let time = endDate - newDate;
+                    if (time >= 0) {
+                        this.days = this.format(Math.floor(time / (1000 * 60 * 60 * 24)));
+                        this.hours = this.format(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+                        this.minutes = this.format(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+                        this.seconds = this.format(Math.floor((time % (1000 * 60)) / 1000));
+                    }
+                }, 1000)
+            },
+            format(num) {
+                return num < 10 ? '0' + num : num;
+            }
         }))
-    })
-    // Text Typeing 
+})
+
+
+
+// Text Typeing 
 
 
 const textElement = document.querySelector('#text');
